fd075e2de745601c203e7f898c623de4c2714fd5
vinz <vinz.chollet@gmail.com>
2017-02-27 16:51:40 +0100

    Read the received messages

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.get("/hello", (req, res) => {
     res.send("Hello World!");
 });
 
-//Chatbot root
+//Chatbot token authentication root
 app.get("/webhook", (req, res) => {
     if ( req.query['hub.mode'] === 'subscribe' &&
         req.query['hub.verify_token'] === "hubert_token" ) {
@@ -34,6 +34,42 @@ app.get("/webhook", (req, res) => {
         res.sendStatus(403);
     }
 });
+
+//Chatbot
+app.post('/webhook', (req, res) => {
+    let data = req.body;
+
+    // Make sure this is a page subscription
+    if (data.object === 'page') {
+
+        // Iterate over each entry - there may be multiple if batched
+        data.entry.forEach(function(entry) {
+            let pageID = entry.id;
+            let timeOfEvent = entry.time;
+
+            // Iterate over each messaging event
+            entry.messaging.forEach(function(event) {
+                if (event.message) {
+                    receivedMessage(event);
+                } else {
+                    console.log("Webhook received unknown event: ", event);
+                }
+            });
+        });
+
+        // Assume all went well.
+        //
+        // You must send back a 200, within 20 seconds, to let us know
+        // you've successfully received the callback. Otherwise, the request
+        // will time out and we will keep trying to resend.
+        res.sendStatus(200);
+    }
+});
+
+function receivedMessage(event) {
+    // Putting a stub for now, we'll expand it in the following steps
+    console.log("Message data: ", event.message);
+}
 
 //Start the server
 app.listen(process.env.PORT || 3000);
